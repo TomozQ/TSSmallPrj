a15a0caa68245b7fc5c0e48e274621d8b60e352a
TomozQ <inf.4clover@gmail.com>
2021-11-21 20:40:19 +0900

    form

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,21 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, __, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -8,20 +25,48 @@ class ProjectInput {
         this.element.id = 'user-input';
         this.titleInputElement = this.element.querySelector('#title');
         this.descriptionInputElement = this.element.querySelector('#description');
-        this.mandayElement = this.element.querySelector('#manday');
+        this.mandayInputElement = this.element.querySelector('#manday');
         this.configure();
         this.attach();
+    }
+    gatherUserInput() {
+        const enterdTitle = this.titleInputElement.value;
+        const enterddescription = this.descriptionInputElement.value;
+        const enterdmanday = this.mandayInputElement.value;
+        if (enterdTitle.trim().length === 0 ||
+            enterddescription.trim().length === 0 ||
+            enterdmanday.trim().length === 0) {
+            alert('入力値が正しくありません。再度お試しください。');
+            return;
+        }
+        else {
+            return [enterdTitle, enterddescription, +enterdmanday];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.mandayInputElement.value = '';
     }
     submitHandler(event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
     }
     configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
+//autoBindDecorator
+function autobind(
+    _: any, 
+    __: string, 
+    descriptor: PropertyDescriptor
+){
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get(){
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+//projectInputClass
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
     titleInputElement: HTMLInputElement
     descriptionInputElement: HTMLInputElement
-    mandayElement: HTMLInputElement
+    mandayInputElement: HTMLInputElement
 
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
@@ -17,20 +35,53 @@ class ProjectInput {
 
         this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
         this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
-        this.mandayElement = this.element.querySelector('#manday') as HTMLInputElement
+        this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement
 
         this.configure()
         this.attach()
     }
 
+    private gatherUserInput(): [string, string, number] | void{    //タプル型の返却値
+        const enterdTitle = this.titleInputElement.value
+        const enterddescription = this.descriptionInputElement.value
+        const enterdmanday = this.mandayInputElement.value
+
+        if(
+            enterdTitle.trim().length === 0 || 
+            enterddescription.trim().length === 0 || 
+            enterdmanday.trim().length === 0
+        ) {
+            alert('入力値が正しくありません。再度お試しください。')
+            return
+        }else{
+            return [ enterdTitle, enterddescription, +enterdmanday ]
+        }
+    }
+
+    private clearInputs(){
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.mandayInputElement.value = ''
+    }
+
+    @autobind
     private submitHandler(event: Event){
         event.preventDefault()
         console.log(this.titleInputElement.value)
+        const userInput = this.gatherUserInput()
+        if(Array.isArray(userInput)){
+            const [title, desc, manday] = userInput
+            console.log(title, desc, manday)
+            this.clearInputs()
+        }
     }
 
     private configure (){
-        this.element.addEventListener('submit', this.submitHandler.bind(this))  //configureメソッドはconstructorから呼ばれる -> ここでのthisはクラスで作成されたインスタンス
-    }                                                                           //submitHandlerにこのthisをbindして渡す
+        this.element.addEventListener('submit', this.submitHandler)
+        // this.element.addEventListener('submit', this.submitHandler.bind(this)) 
+        //configureメソッドはconstructorから呼ばれる -> ここでのthisはクラスで作成されたインスタンス
+        //submitHandlerにこのthisをbindして渡す
+    }                                                                           
 
     private attach(){
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
